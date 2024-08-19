187da91b42e4851889b4b0016622fdacf1e2423b
mateuscardososs <mateus.00000844556@unicap.br>
2024-08-19 08:53:18 -0300

    projeto

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const formElements = document.querySelectorAll('.campo');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    formElements.forEach(element => {
+        observer.observe(element);
+    });
+});
+
